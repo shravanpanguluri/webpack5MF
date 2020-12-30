0595b2f90532c171bb9bb47b9c16f88384f5c4d5
Shravan Panguluri <shravanpanguluri@Shravans-MacBook-Air.local>
2020-12-30 12:56:46 -0500

    added packages

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            Return to home page
           </Typography>
           <Button
             color="primary"
