d7c899c29902f1e1b000ba09e1bda8fb29874279
Shravan Panguluri <shravanpanguluri@Shravans-MacBook-Air.local>
2020-12-30 12:42:38 -0500

    added latest

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/marketing',
+    publicPath: '/marketing/latest',
   },
   plugins: [
     new ModuleFederationPlugin({
