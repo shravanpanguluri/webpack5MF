55898019aeaab6473e633bf419f6f24731d1812c
Shravan Panguluri <shravanpanguluri@Shravans-MacBook-Air.local>
2020-12-30 13:21:30 -0500

    added config files for other projects

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            Return 
+            Return to page
           </Typography>
           <Button
             color="primary"
